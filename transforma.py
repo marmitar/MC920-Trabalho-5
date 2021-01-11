d37c050110443b5449783db7c6b645d003ec4285
Tiago de Paula <tiagodepalves@gmail.com>
2021-01-11 15:45:57 -0300

    Opções de escala

--- transforma.py
+++ transforma.py
@@ -12,11 +12,11 @@ parser = Argumentos(DESCRICAO)
 # modificações na imagem
 parser.add_argument('-a', '--angulo', type=racional,
                     help='rotação da imagem, em graus')
-parser.add_argument('-e', '--escala', type=lambda s: racional(s, min=0),
+escala = parser.add_mutually_exclusive_group()
+escala.add_argument('-e', '--escala', type=lambda s: racional(s, min=0),
                     help='escala de redimensionamento')
-parser.add_argument('-d', '--dim', type=natural, nargs=2,
+escala.add_argument('-d', '--dim', metavar=('ALTURA', 'LARGURA'), type=natural, nargs=2,
                     help='dimensões da imagem resultante')
-# TODO: https://docs.python.org/3/library/argparse.html#mutual-exclusion
 # TODO: opção de cor de fundo
 # entrada e saída
 parser.add_argument('imagem', metavar='IMAGEM', type=imagem, default='-',
