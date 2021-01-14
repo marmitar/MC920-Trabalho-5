c5b8eb3d781425537132f0769a27f04576c2dca4
Tiago de Paula <tiagodepalves@gmail.com>
2021-01-14 15:26:37 -0300

    Rotação com projeção

--- lib/ops.py
+++ lib/ops.py
@@ -13,6 +13,13 @@ def identidade() -> OpLin:
     return np.eye(3, dtype=float)
 
 
+def inversa(mat: OpLin) -> OpLin:
+    """
+    Matriz da transformação inversa.
+    """
+    return np.linalg.pinv(mat)
+
+
 def escalonamento(Sx: float, Sy: Optional[float]=None) -> OpLin:
     """
     Matriz de mudança de escala.
@@ -53,11 +60,12 @@ def translacao(Tx: float, Ty: Optional[float]=None) -> OpLin:
 
 def rotacao(theta: float, graus: bool=True) -> OpLin:
     """
-    Matriz de rotação por um ângulo `theta`.
+    Matriz de rotação no plano xy (em torno do eixo
+    z) por um ângulo `theta`.
     """
     # transformação para radianos
     if graus:
-        theta = (theta * np.pi) / 180
+        theta = np.deg2rad(theta)
     # seno e cosseno
     Ct, St = np.cos(theta), np.sin(theta)
 
@@ -68,8 +76,49 @@ def rotacao(theta: float, graus: bool=True) -> OpLin:
     ], dtype=float)
 
 
-def inversa(mat: OpLin) -> OpLin:
+def rotacao_proj(beta: float, graus: bool=True, D: float=1.0) -> OpLin:
     """
-    Matriz da transformação inversa.
+    Matriz de rotação em torno do eixo y por um ângulo
+    `beta`, projetado novamente no plano xy.
+
+    A imagem é considerada a uma distância `Z = D`
+    e o foco em `Z = 1`.
     """
-    return np.linalg.pinv(mat)
+    # transformação para radianos
+    if graus:
+        beta = np.deg2rad(beta)
+
+    # criação do eixo Z
+    M = np.asarray([
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0],
+        [0, 0, 1]
+    ], dtype=float)
+
+    # translação para Z = D
+    T = np.asarray([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, D],
+        [0, 0, 0, 1]
+    ], dtype=float)
+
+    # seno e cosseno
+    Cb, Sb = np.cos(beta), np.sin(beta)
+    # rotação em torno de y
+    R = np.asarray([
+        [ Cb,  0, Sb,  0],
+        [  0,  1,  0,  0],
+        [-Sb,  0, Cb,  0],
+        [  0,  0,  0,  1]
+    ], dtype=float)
+
+    # projeção com f = 1
+    P = np.asarray([
+        [ 1, 0, 0, 0],
+        [ 0, 1, 0, 1],
+        [ 0, 0,-1, 1]
+    ], dtype=float)
+
+    return P @ R @ T @ M
