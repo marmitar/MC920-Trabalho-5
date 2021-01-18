7f8f3098935fefd8ce999542597972d73460f5d8
Tiago de Paula <tiagodepalves@gmail.com>
2021-01-17 22:42:56 -0300

    Exemplo 2

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 mkdir -p resultados
 
 python3 transforma.py imagens/city.png -o resultados/exemplo.png -a asin\(0.25\) -b deg\(pi/4\) -e 1/3
+
+python3 transforma.py imagens/city.png -o resultados/exemplo2.png -a asin\(0.25\) -b deg\(pi/4\) -e 1/3 -c red -m bicubica
