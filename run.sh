022b0a2625fb9a0103a18abbc90b356e239ad6c2
Tiago de Paula <tiagodepalves@gmail.com>
2021-01-18 20:45:26 -0300

    Reconstrução

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir -p resultados
-# echo Exemplos...
+# mkdir -p resultados
+# # echo Exemplos...
 # python3 transforma.py imagens/city.png -o resultados/exemplo.png -a asin\(0.25\) -b deg\(pi/4\) -e 1/3
 # python3 transforma.py imagens/city.png -o resultados/exemplo2.png -a asin\(0.25\) -b deg\(pi/4\) -e 1/3 -c red -m bicubica
 
-mkdir -p resultados/rotacoes
-# rotações
+# mkdir -p resultados/rotacoes
+# # rotações
 # echo -n Rotações... ALFA...
 # echo -n ' ' house.png...
 # python3 transforma.py imagens/house.png -o resultados/rotacoes/house_alp_viz.png -a 15 -m vizinho
@@ -25,24 +25,47 @@ mkdir -p resultados/rotacoes
 # python3 transforma.py imagens/house64.png -o resultados/rotacoes/64_alp_lag.png -b -30 -m lagrange
 # echo
 
-mkdir -p resultados/escala
+# mkdir -p resultados/escala
+# # escalonamento
+# echo -n Escalonamento...
+# echo -n ' ' 1/3...
+# python3 transforma.py imagens/city.png -o resultados/escala/city_13_viz.png -e 1/3 -m vizinho
+# python3 transforma.py imagens/city.png -o resultados/escala/city_13_bil.png -e 1/3 -m bilinear
+# python3 transforma.py imagens/city.png -o resultados/escala/city_13_bic.png -e 1/3 -m bicubica
+# python3 transforma.py imagens/city.png -o resultados/escala/city_13_lag.png -e 1/3 -m lagrange
+# echo -n ' ' 80x60...
+# python3 transforma.py imagens/city128.png -o resultados/escala/128_86_viz.png -d 80 60 -m vizinho
+# python3 transforma.py imagens/city128.png -o resultados/escala/128_86_bil.png -d 80 60 -m bilinear
+# python3 transforma.py imagens/city128.png -o resultados/escala/128_86_bic.png -d 80 60 -m bicubica
+# python3 transforma.py imagens/city128.png -o resultados/escala/128_86_lag.png -d 80 60 -m lagrange
+# echo -n ' ' 1.5...
+# python3 transforma.py imagens/city128.png -o resultados/escala/128_15_viz.png -e 1.5 -m vizinho
+# python3 transforma.py imagens/city128.png -o resultados/escala/128_15_bil.png -e 1.5 -m bilinear
+# python3 transforma.py imagens/city128.png -o resultados/escala/128_15_bic.png -e 1.5 -m bicubica
+# python3 transforma.py imagens/city128.png -o resultados/escala/128_15_lag.png -e 1.5 -m lagrange
+# echo
+
+mkdir -p resultados/reconstrucao
 # escalonamento
-echo -n Escalonamento...
-echo -n ' ' 1/3...
-python3 transforma.py imagens/city.png -o resultados/escala/city_13_viz.png -e 1/3 -m vizinho
-python3 transforma.py imagens/city.png -o resultados/escala/city_13_bil.png -e 1/3 -m bilinear
-python3 transforma.py imagens/city.png -o resultados/escala/city_13_bic.png -e 1/3 -m bicubica
-python3 transforma.py imagens/city.png -o resultados/escala/city_13_lag.png -e 1/3 -m lagrange
-echo -n ' ' 80x60...
-python3 transforma.py imagens/city128.png -o resultados/escala/128_86_viz.png -d 80 60 -m vizinho
-python3 transforma.py imagens/city128.png -o resultados/escala/128_86_bil.png -d 80 60 -m bilinear
-python3 transforma.py imagens/city128.png -o resultados/escala/128_86_bic.png -d 80 60 -m bicubica
-python3 transforma.py imagens/city128.png -o resultados/escala/128_86_lag.png -d 80 60 -m lagrange
-echo -n ' ' 1.5...
-python3 transforma.py imagens/city128.png -o resultados/escala/128_15_viz.png -e 1.5 -m vizinho
-python3 transforma.py imagens/city128.png -o resultados/escala/128_15_bil.png -e 1.5 -m bilinear
-python3 transforma.py imagens/city128.png -o resultados/escala/128_15_bic.png -e 1.5 -m bicubica
-python3 transforma.py imagens/city128.png -o resultados/escala/128_15_lag.png -e 1.5 -m lagrange
+echo -n Reconstrução...
+echo -n ' ' x2...
+python3 transforma.py imagens/baboon.png -o - -e 2 -m vizinho | \
+  python3 transforma.py - -o resultados/reconstrucao/baboon_x2_viz.png -e 1/2 -m vizinho
+python3 transforma.py imagens/baboon.png -o - -e 2 -m bilinear | \
+  python3 transforma.py - -o resultados/reconstrucao/baboon_x2_bil.png -e 1/2 -m bilinear
+python3 transforma.py imagens/baboon.png -o - -e 2 -m bicubica | \
+  python3 transforma.py - -o resultados/reconstrucao/baboon_x2_bic.png -e 1/2 -m bicubica
+python3 transforma.py imagens/baboon.png -o - -e 2 -m lagrange | \
+  python3 transforma.py - -o resultados/reconstrucao/baboon_x2_lag.png -e 1/2 -m lagrange
+echo -n ' ' rot45...
+python3 transforma.py imagens/baboon.png -o - -a 45 -m vizinho | \
+  python3 transforma.py - -o resultados/reconstrucao/baboon_45_viz.png -a -45 -m vizinho
+python3 transforma.py imagens/baboon.png -o - -a 45 -m bilinear | \
+  python3 transforma.py - -o resultados/reconstrucao/baboon_45_bil.png -a -45 -m bilinear
+python3 transforma.py imagens/baboon.png -o - -a 45 -m bicubica | \
+  python3 transforma.py - -o resultados/reconstrucao/baboon_45_bic.png -a -45 -m bicubica
+python3 transforma.py imagens/baboon.png -o - -a 45 -m lagrange | \
+  python3 transforma.py - -o resultados/reconstrucao/baboon_45_lag.png -a -45 -m lagrange
 echo
 
 # profiling
