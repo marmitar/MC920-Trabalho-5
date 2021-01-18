55e1b4bf1a07adb10351873b3c87ea9dac482ec0
Tiago de Paula <tiagodepalves@gmail.com>
2021-01-18 17:59:28 -0300

    Profiling

--- run.sh
+++ run.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
 mkdir -p resultados
-
+echo Exemplos...
 python3 transforma.py imagens/city.png -o resultados/exemplo.png -a asin\(0.25\) -b deg\(pi/4\) -e 1/3
-
 python3 transforma.py imagens/city.png -o resultados/exemplo2.png -a asin\(0.25\) -b deg\(pi/4\) -e 1/3 -c red -m bicubica
+
+# profiling
+echo -n Profiling...
+echo -n ' ' vizinho...
+python3 -m cProfile -s cumtime transforma.py imagens/among.png -o saida.png -c r -a 22 -e 2 -b 20 -m vizinho > resultados/vizinho.txt
+echo -n ' ' bilinear...
+python3 -m cProfile -s cumtime transforma.py imagens/among.png -o saida.png -c r -a 22 -e 2 -b 20 -m bilinear > resultados/bilinear.txt
+echo -n ' ' bicubica...
+python3 -m cProfile -s cumtime transforma.py imagens/among.png -o saida.png -c r -a 22 -e 2 -b 20 -m bicubica > resultados/bicubica.txt
+echo -n ' ' lagrange...
+python3 -m cProfile -s cumtime transforma.py imagens/among.png -o saida.png -c r -a 22 -e 2 -b 20 -m lagrange > resultados/lagrange.txt
+echo
