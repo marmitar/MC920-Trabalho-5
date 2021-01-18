0d5b875729c0d19df88bf2828558feeb1088ca3b
Tiago de Paula <tiagodepalves@gmail.com>
2021-01-18 18:31:52 -0300

    House 16x16

--- run.sh
+++ run.sh
@@ -8,11 +8,16 @@
 mkdir -p resultados/rotacoes
 # rotações ALFA
 echo -n Rotações ALFA...
-echo -n ' ' house.png...
-python3 transforma.py imagens/house.png -o resultados/rotacoes/house_alp_viz.png -a 15 -m vizinho
-python3 transforma.py imagens/house.png -o resultados/rotacoes/house_alp_bil.png -a 15 -m bilinear
-python3 transforma.py imagens/house.png -o resultados/rotacoes/house_alp_bic.png -a 15 -m bicubica
-python3 transforma.py imagens/house.png -o resultados/rotacoes/house_alp_lag.png -a 15 -m lagrange
+# echo -n ' ' house.png...
+# python3 transforma.py imagens/house.png -o resultados/rotacoes/house_alp_viz.png -a 15 -m vizinho
+# python3 transforma.py imagens/house.png -o resultados/rotacoes/house_alp_bil.png -a 15 -m bilinear
+# python3 transforma.py imagens/house.png -o resultados/rotacoes/house_alp_bic.png -a 15 -m bicubica
+# python3 transforma.py imagens/house.png -o resultados/rotacoes/house_alp_lag.png -a 15 -m lagrange
+echo -n ' ' house16.png...
+python3 transforma.py imagens/house16.png -o resultados/rotacoes/16_alp_viz.png -a 15 -m vizinho
+python3 transforma.py imagens/house16.png -o resultados/rotacoes/16_alp_bil.png -a 15 -m bilinear
+python3 transforma.py imagens/house16.png -o resultados/rotacoes/16_alp_bic.png -a 15 -m bicubica
+python3 transforma.py imagens/house16.png -o resultados/rotacoes/16_alp_lag.png -a 15 -m lagrange
 echo
 
 # profiling
