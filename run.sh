d6633c79a1f6f2832e10f2ffa972e81760c37a46
Tiago de Paula <tiagodepalves@gmail.com>
2021-01-18 20:02:40 -0300

    Figuras escalonadas

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# mkdir -p resultados
+mkdir -p resultados
 # echo Exemplos...
 # python3 transforma.py imagens/city.png -o resultados/exemplo.png -a asin\(0.25\) -b deg\(pi/4\) -e 1/3
 # python3 transforma.py imagens/city.png -o resultados/exemplo2.png -a asin\(0.25\) -b deg\(pi/4\) -e 1/3 -c red -m bicubica
 
 mkdir -p resultados/rotacoes
-# rotações ALFA
-echo -n Rotações... ALFA...
+# rotações
+# echo -n Rotações... ALFA...
 # echo -n ' ' house.png...
 # python3 transforma.py imagens/house.png -o resultados/rotacoes/house_alp_viz.png -a 15 -m vizinho
 # python3 transforma.py imagens/house.png -o resultados/rotacoes/house_alp_bil.png -a 15 -m bilinear
@@ -18,11 +18,31 @@ echo -n Rotações... ALFA...
 # python3 transforma.py imagens/house16.png -o resultados/rotacoes/16_alp_bil.png -a 15 -m bilinear
 # python3 transforma.py imagens/house16.png -o resultados/rotacoes/16_alp_bic.png -a 15 -m bicubica
 # python3 transforma.py imagens/house16.png -o resultados/rotacoes/16_alp_lag.png -a 15 -m lagrange
-echo -n ' ' beta...
-python3 transforma.py imagens/house64.png -o resultados/rotacoes/64_alp_viz.png -b -30 -m vizinho
-python3 transforma.py imagens/house64.png -o resultados/rotacoes/64_alp_bil.png -b -30 -m bilinear
-python3 transforma.py imagens/house64.png -o resultados/rotacoes/64_alp_bic.png -b -30 -m bicubica
-python3 transforma.py imagens/house64.png -o resultados/rotacoes/64_alp_lag.png -b -30 -m lagrange
+# echo -n ' ' beta...
+# python3 transforma.py imagens/house64.png -o resultados/rotacoes/64_alp_viz.png -b -30 -m vizinho
+# python3 transforma.py imagens/house64.png -o resultados/rotacoes/64_alp_bil.png -b -30 -m bilinear
+# python3 transforma.py imagens/house64.png -o resultados/rotacoes/64_alp_bic.png -b -30 -m bicubica
+# python3 transforma.py imagens/house64.png -o resultados/rotacoes/64_alp_lag.png -b -30 -m lagrange
+# echo
+
+mkdir -p resultados/escala
+# escalonamento
+echo -n Escalonamento...
+echo -n ' ' 1/4...
+python3 transforma.py imagens/city.png -o resultados/escala/city_14_viz.png -e 1/4 -m vizinho
+python3 transforma.py imagens/city.png -o resultados/escala/city_14_bil.png -e 1/4 -m bilinear
+python3 transforma.py imagens/city.png -o resultados/escala/city_14_bic.png -e 1/4 -m bicubica
+python3 transforma.py imagens/city.png -o resultados/escala/city_14_lag.png -e 1/4 -m lagrange
+echo -n ' ' 80x60...
+python3 transforma.py imagens/city128.png -o resultados/escala/128_86_viz.png -d 80 60 -m vizinho
+python3 transforma.py imagens/city128.png -o resultados/escala/128_86_bil.png -d 80 60 -m bilinear
+python3 transforma.py imagens/city128.png -o resultados/escala/128_86_bic.png -d 80 60 -m bicubica
+python3 transforma.py imagens/city128.png -o resultados/escala/128_86_lag.png -d 80 60 -m lagrange
+echo -n ' ' 1.5...
+python3 transforma.py imagens/city128.png -o resultados/escala/128_2_viz.png -e 1.5 -m vizinho
+python3 transforma.py imagens/city128.png -o resultados/escala/128_2_bil.png -e 1.5 -m bilinear
+python3 transforma.py imagens/city128.png -o resultados/escala/128_2_bic.png -e 1.5 -m bicubica
+python3 transforma.py imagens/city128.png -o resultados/escala/128_2_lag.png -e 1.5 -m lagrange
 echo
 
 # profiling
